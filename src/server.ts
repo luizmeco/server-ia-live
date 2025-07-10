e3cc1bf3515e1d48f5a8561e074bcdd90728a866
Luiz Américo Valandro <meco.valandro@gmail.com>
2025-07-09 22:02:29 -0300

    adicionado as rotas do server e configs do db

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import { fastify } from "fastify";
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifyCors } from "@fastify/cors";
+import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: '*', // Allow all origins
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+    return 'OK';
+});
+
+app.register(getRoomsRoute)
+
+app.listen({ port: env.PORT}).then(() => {
+    console.log(`Server is running on http://localhost:${env.PORT}`);
+})
